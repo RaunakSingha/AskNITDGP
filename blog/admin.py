00ded2e0f1f1efc1930294818239f189018d9d37
RaunakSingha <rksingha1212@gmail.com>
2017-10-01 22:08:50 +0530

    Modified admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,17 +2,5 @@ from django.contrib import admin
 from . models import Post, Comment
 
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publish',)
-    list_filter = ('publish', 'author')
-    search_fields = ('title', 'body', 'author')
-    date_hierarchy = 'publish'
-    ordering = ['-publish']
-admin.site.register(Post, PostAdmin)
-
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'post', 'publish')
-    list_filter = ('publish', )
-    search_fields = ('name', 'email', 'body')
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(Post)
+admin.site.register(Comment)
